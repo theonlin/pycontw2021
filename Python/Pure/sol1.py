c0ed1d38dd2deecec768bc498dc386e087463402
Theon Lin <theon.lin@greatergood.com.tw>
2021-04-23 05:04:21 +0000

    Remove debug message.

--- Python/Pure/sol1.py
+++ Python/Pure/sol1.py
@@ -2,7 +2,6 @@ import sys
 from fib import fib
 
 if __name__ == '__main__':
-    print(sys.argv)
     if len(sys.argv) > 1:
         n = int(sys.argv[1])
         print("Fib({}) = {}".format(n, fib(n)))
